a0c5113d15e31cb942d3c0445697eb9b79e54803
Gabriel Faria <gabriel.faria@resultate.com.br>
2015-09-23 11:05:30 -0300

    Alteracoes de estilo na pagina de inclusao de projetos

--- resources/views/projetos/incluir.blade.php
+++ resources/views/projetos/incluir.blade.php
@@ -4,9 +4,9 @@
     <h1 class="titulo">CADASTRO DE PROJETO</h1>
     <hr/>
 
-    <form action="">
+    <form action="" class="big-form form-com-divisoria">
         <div class="row">
-            <div class="col-sm-offset-1 col-sm-5 col-md-offset-2 col-md-4">
+            <div class="col-sm-6 col-md-offset-1 col-md-5 esquerda">
                 <div class="form-group">
                     <label for="nome">Nome do Projeto</label>
                     <input id="nome" name="nome" type="text" class="form-control"/>
@@ -24,11 +24,10 @@
                     <input id="imagem" name="imagem" type="file" class="form-control"/>
                 </div>
             </div>
-            <div class="col-sm-5 col-md-4">
+            <div class="col-sm-6 col-md-5 direita">
                 <div class="form-group">
                     <label for="descricao">Descrição do Projeto</label>
-                    <textarea class="form-control" name="descricao" id="descricao" cols="30" rows="10">
-                    </textarea>
+                    <textarea class="form-control big-textarea" name="descricao" id="descricao"></textarea>
                 </div>
                 <div class="form-group">
                     <button class="btn btn-primary btn-block">
